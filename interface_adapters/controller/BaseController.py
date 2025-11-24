eb92854e89470d9a15b88d79b81925b2971c5357
Romualdo Massuia Jr <juniormassuia@gmail.com>
2025-11-24 11:54:36 -0400

    commit

---
+++ interface_adapters/controller/BaseController.py
@@ -0,0 +1,45 @@
+from abc import ABC, abstractmethod
+from sessao_handle import set_handle_atual, limpar_handle_atual
+
+
+class BaseController(ABC):
+    def __init__(self, handle: int):
+        self.handle = handle
+        self._prepared = False
+
+    def execute(self):
+        """
+        Este método é executado dentro da thread criada pelo MainApp.
+        Aqui configuramos o handle da thread e chamamos o "construtor de thread".
+        """
+        set_handle_atual(self.handle)
+
+        try:
+            if not self._prepared:
+                self._prepare()     # "construtor de thread"
+                self._prepared = True
+
+            self._run()            # lógica principal do controller
+
+        finally:
+            limpar_handle_atual()
+
+    @abstractmethod
+    def _prepare(self):
+        """
+        Construtor interno da thread.
+        Tudo que depende de get_handle_atual() deve ser instanciado aqui.
+        """
+        pass
+
+    @abstractmethod
+    def _run(self):
+        """Lógica principal da thread."""
+        pass
+
+    def parar(self):
+        """Opcional: pode ser sobrescrito."""
+        pass
+
+    def parar(self):
+        pass
