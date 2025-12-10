667fe16174282462a7aa41ddb4cf5e1bbe42b059
Romualdo Massuia Jr <juniormassuia@gmail.com>
2025-12-10 15:01:00 -0400

    commit

---
+++ interface_adapters/teste/controller/teste_controller.py
@@ -0,0 +1,21 @@
+import win32gui
+
+from interface_adapters.controller.BaseController import BaseController
+from interface_adapters.up.up_util.up_util import Up_util
+from utils.mover_spot_util import MoverSpotUtil
+from utils.pointer_util import Pointers
+from utils.teclado_util import Teclado_util
+
+
+class TesteController(BaseController):
+
+    def _prepare(self):
+        self.pointer = Pointers()
+        self.up_util = Up_util()
+        self.mover_spot = MoverSpotUtil()
+        self.classe = self.pointer.get_classe()
+        self.tela = win32gui.GetWindowText(self.handle)
+        self.teclado_util = Teclado_util()
+
+    def _run(self):
+        self.mover_spot.movimentar((119, 152), max_tempo=600, limpar_spot_se_necessario=True)
